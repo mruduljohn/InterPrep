0a17ab30bb54d5282b9b7cb1c9da3c1e2f17ee24
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-11-11 01:46:59 +0530

    Fixes 1

--- Homepage.py
+++ Homepage.py
@@ -4,23 +4,17 @@ from app_utils import switch_page
 from PIL import Image
 
 im = Image.open("icon.png")
-st.set_page_config(page_title="AI Interviewer", layout="centered", page_icon=im)
+st.set_page_config(page_title="InterPrep AI", layout="centered", page_icon=im)
 
-lan = st.selectbox("#### Language", ["English", "中文"])
+lan = st.selectbox("#### Language", ["English"])
 
 if lan == "English":
-    home_title = "AI Interviewer"
-    home_introduction = "Welcome to AI Interviewer, empowering your interview preparation with generative AI."
+    home_title = "InterPrep AI"
+    home_introduction = "Welcome to InterPrep AI, empowering your interview preparation with generative AI."
     with st.sidebar:
-        st.markdown('AI Interviewer - V0.1.2')
         st.markdown(""" 
         #### Let's contact:
-        [Haoxiang Jia](https://www.linkedin.com/in/haoxiang-jia/)
-        
-        [Zicheng Wang](https://www.linkedin.com/in/todd-wang-5001aa264/)
-        #### Please fill the form, we'd love to have your feedback:
-        [Feedback Form](https://docs.google.com/forms/d/13f4q03bk4lD7sKR7qZ8UM1lQDo6NhRaAKv7uIeXHEaQ/edit)
-    
+        [Mrudul John](https://www.linkedin.com/in/mruduljohnmathews/)
         #### Powered by
     
         [OpenAI](https://openai.com/)
@@ -35,18 +29,10 @@ if lan == "English":
         unsafe_allow_html=True
     )
     st.image(im, width=100)
-    st.markdown(f"""# {home_title} <span style=color:#2E9BF5><font size=5>Beta</font></span>""", unsafe_allow_html=True)
+    st.markdown(f"""# {home_title} """, unsafe_allow_html=True)
     st.markdown("""\n""")
-    st.markdown("Welcome to AI Interviewer! 👏 AI Interviewer is your personal interviewer powered by generative AI that conducts mock interviews."
-                "You can upload your resume and enter job descriptions, and AI Interviewer will ask you customized questions. Additionally, you can configure your own Interviewer!")
-    st.markdown("""\n""")
-    with st.expander("Updates"):
-        st.write("""
-        08/13/2023
-        - Fix the error that occurs when the user input fails to be recorded. """)
-    with st.expander("What's coming next?"):
-        st.write("""
-        Improved voice interaction for a seamless experience. """)
+    st.markdown("Welcome to InterPrep AI! 👏 This is your personal interviewer powered by generative AI that conducts mock interviews and gives you suggestions."
+                "You can upload your resume and enter job descriptions, and InterPrep AI will ask you customized questions. Additionally, you can configure your own Interviewer!")
     st.markdown("""\n""")
     st.markdown("#### Get started!")
     st.markdown("Select one of the following screens to start your interview!")
@@ -59,46 +45,37 @@ if lan == "English":
     )
     if selected == 'Professional':
         st.info("""
-            📚In this session, the AI Interviewer will assess your technical skills as they relate to the job description.
+            📚In this mode, the InterPrep AI will assess your technical skills as they relate to the job description.
             Note: The maximum length of your answer is 4097 tokens!
             - Each Interview will take 10 to 15 mins.
             - To start a new session, just refresh the page.
-            - Choose your favorite interaction style (chat/voice)
             - Start introduce yourself and enjoy！ """)
         if st.button("Start Interview!"):
             switch_page("Professional Screen")
     if selected == 'Resume':
         st.info("""
-        📚In this session, the AI Interviewer will review your resume and discuss your past experiences.
+        📚In this session, the InterPrep AI will review your resume and discuss your past experiences.
         Note: The maximum length of your answer is 4097 tokens!
         - Each Interview will take 10 to 15 mins.
         - To start a new session, just refresh the page.
-        - Choose your favorite interaction style (chat/voice)
         - Start introduce yourself and enjoy！ """
                 )
         if st.button("Start Interview!"):
             switch_page("Resume Screen")
     if selected == 'Behavioral':
         st.info("""
-        📚In this session, the AI Interviewer will assess your soft skills as they relate to the job description.
+        📚In this session, the InterPrep AI will assess your soft skills as they relate to the job description.
         Note: The maximum length of your answer is 4097 tokens!
         - Each Interview will take 10 to 15 mins.
         - To start a new session, just refresh the page.
-        - Choose your favorite interaction style (chat/voice)
         - Start introduce yourself and enjoy！ 
         """)
         if st.button("Start Interview!"):
             switch_page("Behavioral Screen")
     if selected == 'Customize!':
         st.info("""
-            📚In this session, you can customize your own AI Interviewer and practice with it!
+            📚In this session, you can customize your own InterPrep AI and practice with it!
              - Configure AI Interviewer in different specialties.
-             - Configure AI Interviewer in different personalities.
-             - Different tones of voice.
-             
-             Coming at the end of July""")
-    st.markdown("""\n""")
-    st.markdown("#### Wiki")
-    st.write('[Click here to view common FAQs, future updates and more!](https://jiatastic.notion.site/wiki-8d962051e57a48ccb304e920afa0c6a8?pvs=4)')
+             - Configure AI Interviewer in different personalities.""")
 
 # The rest of your code...
